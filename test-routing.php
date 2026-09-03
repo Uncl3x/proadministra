7c91f12f143076b06c4a06a44e178a742e0f3541
Nicolas Moreno <cross.noas@gmail.com>
2026-09-03 12:43:44 -0400

    clean routing test

--- test-routing.php
+++
@@ -1,5 +0,0 @@
-<?php
-$local = @file_get_contents('/etc/localdomains');
-$remote = @file_get_contents('/etc/remotedomains');
-echo "Local domains contains proadministra.cl: " . (strpos($local, 'proadministra.cl') !== false ? 'YES' : 'NO') . "\n";
-echo "Remote domains contains proadministra.cl: " . (strpos($remote, 'proadministra.cl') !== false ? 'YES' : 'NO') . "\n";
